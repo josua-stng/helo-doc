c0d0074c9e92a33ebf1be715f4a7f3ac34f07dc9
josua-stng <josuasitanggang51@gmail.com>
2023-04-19 12:32:21 +0700

    fix button style

--- src/components/DoctorOptions.tsx
+++ src/components/DoctorOptions.tsx
@@ -1,11 +1,11 @@
 const DoctorOptions = () => {
   return (
     <div className="grid justify-around grid-cols-3 gap-2 md:grid-cols-5 mt-4">
-      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-9 md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">See Doctors</button>
-      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-9 md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Market</button>
-      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-9 md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Book Doctor</button>  
-      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-9 md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">See Hospital</button>
-      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-9 md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Chat Doctor</button>
+      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-max-content md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">See Doctors</button>
+      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-max-content md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Market</button>
+      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-max-content md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Book Doctor</button>  
+      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-max-content md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">See Hospital</button>
+      <button className="m-2 p-1 drop-shadow-lg bg-slate-100 h-max-content md:h-11 hover:bg-slate-200 hover:drop-shadow-xl">Chat Doctor</button>
     </div>
   );
 };
