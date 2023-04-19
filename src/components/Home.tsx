a5aef06763d83508cebf3d1b618604bf4eb10197
josua-stng <josuasitanggang51@gmail.com>
2023-04-19 12:26:07 +0700

    create home-page menu

---
+++ src/components/Home.tsx
@@ -0,0 +1,14 @@
+import DoctorOptions from "./DoctorOptions"
+import Navbar from "./Navbar"
+
+const Home =() =>{
+    return(
+        <div>
+            <Navbar/>
+            <DoctorOptions/>
+            <h1>Home Page Menu</h1>
+        </div>
+    )
+}
+
+export default Home
