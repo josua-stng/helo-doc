21adf2b962e878052c82b03e0ecc5964affabd92
josua-stng <josuasitanggang51@gmail.com>
2023-04-20 13:35:44 +0700

    create route for doctor list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import SeeDoctor from "./components/SeeDoctors";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/doctor-list" element={<SeeDoctor/>}/>
       </Routes>
     </div>
   );
