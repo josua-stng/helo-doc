c5726aee8dce9523a9e3503326a854f35a0b3c70
josua-stng <josuasitanggang51@gmail.com>
2023-04-24 14:09:16 +0700

    create market component

---
+++ src/components/Market.tsx
@@ -0,0 +1,13 @@
+import MarketOption from "../option/MarketOpton";
+import Navbar from "./Navbar";
+
+const Market = () => {
+  return (
+    <div>
+      <Navbar />
+      <MarketOption/>
+    </div>
+  );
+};
+
+export default Market;
