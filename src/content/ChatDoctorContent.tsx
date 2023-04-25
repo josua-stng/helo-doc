51b89a8bc704ed25764bb9d9e590a43288879326
josua-stng <josuasitanggang51@gmail.com>
2023-04-25 13:53:43 +0700

    create props for chat doctor

---
+++ src/content/ChatDoctorContent.tsx
@@ -0,0 +1,27 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+type dataProps={
+    onChange:React.ChangeEventHandler<HTMLInputElement>,
+    onSubmit:(event: React.FormEvent<HTMLFormElement>) => void,
+    Value:string
+}
+const ChatDoctor = ({onChange,Value,onSubmit}:dataProps) => {
+    
+  return (
+    <div>
+      <form className="text-center mt-5 text-1xl" onSubmit={onSubmit}>
+        <div className="w-full relative px-10 md:px-20">
+          <input
+            type="text"
+            placeholder="Search Doctor"
+            value={Value}
+            onChange={onChange}
+            className="border-2 border-gray-300 w-full h-10 pl-2 rounded-lg "
+          />
+          <MagnifyingGlassIcon className="w-5 absolute right-14 md:right-24 top-1/4" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default ChatDoctor;
