cb9649e973bee80f55a2710aa1cccba3f31213b5
josua-stng <josuasitanggang51@gmail.com>
2023-04-26 10:13:19 +0700

    create component calculate BMI

---
+++ src/components/CalculatorBMI.tsx
@@ -0,0 +1,15 @@
+import CalculatorBMIContent from "../content/CalculatorBMIContext"
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+
+const CalculatorBMI = ()=>{
+    return(
+        <div>
+            <Navbar/>
+            <CalculatorBMIContent/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default CalculatorBMI
