17b23d6a139d638852357e9ff73083cef3b30d6e
josua-stng <josuasitanggang51@gmail.com>
2023-04-27 12:15:04 +0700

    create see hospital component

---
+++ src/components/SeeHospital.tsx
@@ -0,0 +1,12 @@
+import SeeHospitalContent from "../content/SeeHospitalContent";
+import Navbar from "./Navbar";
+
+const SeeHospital = () => {
+  return (
+    <div>
+      <Navbar />
+      <SeeHospitalContent/>
+    </div>
+  );
+};
+export default SeeHospital;
